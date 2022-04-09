d2773f2b55c69d286441df49924208f0e71b4266
RoozbehZhuleh <zhuleh.developer@gmail.com>
2022-04-09 18:49:57 +0430

    final

---
+++ script/script.js
@@ -0,0 +1,79 @@
+const API_URL = "https://api.github.com/users/";
+
+let btnSearch = document.querySelector("#btn-search");
+let inputSearch = document.querySelector("#username");
+let imgAvatar = document.querySelector("#avatar");
+let realName = document.querySelector("#real-name");
+let isHireable = document.querySelector("#hire");
+let twitterHandle = document.querySelector("#twitter");
+let followerNumber = document.querySelector("#followers");
+let followingNumber = document.querySelector("#following");
+let gitBio = document.querySelector("#git-bio");
+
+btnSearch.addEventListener("click", getData);
+
+
+function getData() {
+    let searchTerm = inputSearch.value;
+    fetch(API_URL + searchTerm, { method: "GET"})
+    .then (function (response) {
+        return response.json();
+    })
+    .then (function (data){
+      if (data.message==="Not Found") {
+        toasti("User not found!");
+        return;
+      }
+
+      else {
+        render(data);
+
+      }
+    })
+
+
+
+  function render(data) {
+    const {avatar_url , hireable, following, followers, location, name, twitter_username, public_repos, bio} = data;
+    console.log(data);
+    realName.textContent = data.name;
+    imgAvatar.src = data.avatar_url;
+    isHireable = data.hireable;
+    followerNumber.innerHTML =`${data.followers} followers`;
+    followingNumber.innerHTML = `${data.following} following`;
+    
+    if(bio == null){
+      gitBio.innerHTML = "No bio!";
+      gitBio.className = "git-bio-none"
+    }
+    else {
+      gitBio.innerHTML = data.bio;
+      gitBio.className ="git-bio";
+    }
+    
+    if (twitter_username == null){ 
+      twitterHandle.innerHTML = `no twitter account`
+    }
+    else{
+      twitterHandle.textContent = `@${twitter_username}`;
+    }
+
+} 
+};
+
+
+// toastify 
+const toasti = (textValue) => {
+  Toastify({
+      text: textValue,
+      duration: 2000,
+      newWindow: true,
+      className: "info",
+      close: true,
+      gravity: "top",
+      position: "right",
+      onClick: function() {
+
+      }
+  }).showToast();
+}
